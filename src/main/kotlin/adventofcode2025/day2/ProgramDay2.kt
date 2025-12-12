909c0d0af3433e03d568e8b6b0b25c0119ed334b
Florian KAISER <florian_kaiser@ext.connect-tech.sncf>
2025-12-12 16:16:27 +0100

    day2

--- src/main/kotlin/adventofcode2025/day2/ProgramDay2.kt
+++ src/main/kotlin/adventofcode2025/day2/ProgramDay2.kt
@@ -3,9 +3,42 @@ package adventofcode2025.day2
 import common.Program
 
 class ProgramDay2(brutInputs: List<String>, private val debug: Boolean = false) : Program {
-    private val inputs = brutInputs
+    private val ranges = brutInputs.first().split(",").map {
+        it.split("-").let { LongRange(it[0].toLong(), it[1].toLong()) }
+    }
 
-    override fun part1(): String = ""
+    override fun part1(): String = ranges.flatMap { it.getAllSymetricsValue() }.sum().toString()
 
-    override fun part2(): String = ""
+    override fun part2(): String = ranges.flatMap { it.getInvalids() }.sum().toString()
+}
+
+private fun LongRange.getInvalids(): List<Long> {
+    return this.mapNotNull {
+        it.takeIf {
+            it.toString().let { stringValue ->
+                (stringValue.length % 2 == 0 &&
+                        stringValue.take((stringValue.length / 2)) == stringValue.takeLast(stringValue.length / 2)) ||
+                        stringValue.isRepeated()
+            }
+        }
+    }
+}
+
+private fun String.isRepeated(repeatShema: Int = 2): Boolean {
+    if(repeatShema > length) return false
+    if(this.length % repeatShema != 0) return isRepeated(repeatShema + 1)
+    val size = length / repeatShema
+    if(this.replace(this.take(size), "").isEmpty()) return true
+    return isRepeated(repeatShema + 1)
+}
+
+private fun LongRange.getAllSymetricsValue(): List<Long> {
+    return this.mapNotNull {
+        it.takeIf {
+            it.toString().let { stringValue ->
+                stringValue.length % 2 == 0 &&
+                        stringValue.take((stringValue.length / 2)) == stringValue.takeLast(stringValue.length / 2)
+            }
+        }
+    }
 }
--- src/test/kotlin/adventofcode2025/day2/Day2KtTest.kt
+++ src/test/kotlin/adventofcode2025/day2/Day2KtTest.kt
@@ -6,7 +6,7 @@ import org.junit.Test
 internal class Day2KtTest {
     private val program = ProgramDay2(
         brutInputs = listOf(
-            ""
+            "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124",
         ).map { it },
         debug = true
     )
@@ -14,7 +14,7 @@ internal class Day2KtTest {
     @Test
     fun part1() {
         assertEquals(
-            expected = "",
+            expected = "1227775554",
             actual = program.part1()
         )
     }
@@ -22,7 +22,7 @@ internal class Day2KtTest {
     @Test
     fun part2() {
         assertEquals(
-            expected = "",
+            expected = "4174379265",
             actual = program.part2()
         )
     }
