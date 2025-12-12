fa823ff4c74b104399947e00189b2cc33433b47e
Florian KAISER <florian_kaiser@ext.connect-tech.sncf>
2025-12-13 00:18:15 +0100

    day6

--- src/main/kotlin/adventofcode2025/day6/ProgramDay6.kt
+++ src/main/kotlin/adventofcode2025/day6/ProgramDay6.kt
@@ -1,11 +1,62 @@
 package adventofcode2025.day6
 
 import common.Program
+import common.multiply
+import kotlin.collections.mapIndexed
+import kotlin.text.toLong
 
 class ProgramDay6(brutInputs: List<String>, private val debug: Boolean = false) : Program {
-    private val inputs = brutInputs
+    private val operationsPart1 = brutInputs.map { it.split(" ").filter { it.isNotBlank() } }.let { lines ->
+        val operators = lines.last()
+        val numbers = lines.take(lines.size - 1)
+        operators.mapIndexed { index, operator ->
+            Pair(operator.first(), numbers.map { it[index].toLong() })
+        }
+    }
 
-    override fun part1(): String = ""
+    private val operationsPart2 = brutInputs.let { lines ->
+        val operators = lines.last()
+        val numbers = lines.take(lines.size - 1)
 
-    override fun part2(): String = ""
+        var currentOperator = ' '
+        var currentNumbers = mutableListOf<Long>()
+        val operations = mutableListOf<Pair<Char, List<Long>>>()
+        operators.forEachIndexed { index, operator ->
+            if (operator != ' ') {
+                currentOperator = operator
+                currentNumbers = mutableListOf()
+            }
+
+            val isEmptyColumn = (operator == ' ' && numbers.all { it[index] == ' ' })
+
+            if(!isEmptyColumn) {
+                numbers
+                    .map { it[index] }
+                    .filter { it != ' ' }
+                    .joinToString("")
+                    .toLong()
+                    .let { currentNumbers.add(it) }
+            }
+
+            if (isEmptyColumn || index == operators.length - 1) {
+                operations.add(Pair(currentOperator, currentNumbers))
+            }
+        }
+        operations.toList()
+    }
+
+    override fun part1(): String = operationsPart1.mySum().toString()
+
+    override fun part2(): String = operationsPart2.mySum().toString()
+
+    fun List<Pair<Char, List<Long>>>.mySum() = this.sumOf { operation ->
+        val operator = operation.first
+        val numbers = operation.second
+
+        when (operator) {
+            '+'-> numbers.sum()
+            '*' -> numbers.multiply()
+            else -> error("PROUT")
+        }
+    }
 }
--- src/test/kotlin/adventofcode2025/day6/Day6KtTest.kt
+++ src/test/kotlin/adventofcode2025/day6/Day6KtTest.kt
@@ -6,7 +6,10 @@ import org.junit.Test
 internal class Day6KtTest {
     private val program = ProgramDay6(
         brutInputs = listOf(
-            ""
+            "123 328  51 64 ",
+            " 45 64  387 23 ",
+            "  6 98  215 314",
+            "*   +   *   +  ",
         ).map { it },
         debug = true
     )
@@ -14,7 +17,7 @@ internal class Day6KtTest {
     @Test
     fun part1() {
         assertEquals(
-            expected = "",
+            expected = "4277556",
             actual = program.part1()
         )
     }
@@ -22,7 +25,7 @@ internal class Day6KtTest {
     @Test
     fun part2() {
         assertEquals(
-            expected = "",
+            expected = "3263827",
             actual = program.part2()
         )
     }
