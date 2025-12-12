edf661693e7c6ff26b5ae8d69ec4bebbd5202827
Florian KAISER <florian_kaiser@ext.connect-tech.sncf>
2025-12-12 22:40:30 +0100

    day5

--- src/main/kotlin/adventofcode2025/day5/ProgramDay5.kt
+++ src/main/kotlin/adventofcode2025/day5/ProgramDay5.kt
@@ -1,11 +1,52 @@
 package adventofcode2025.day5
 
 import common.Program
+import common.parseGroupsList
 
 class ProgramDay5(brutInputs: List<String>, private val debug: Boolean = false) : Program {
-    private val inputs = brutInputs
+    private val input = brutInputs.parseGroupsList().let {
+        Input(
+            it.first().map { it.split("-").let { LongRange(it[0].toLong(), it[1].toLong()) } },
+            it.last().map { it.toLong() })
+    }
 
-    override fun part1(): String = ""
+    override fun part1(): String = input.ingredientIds.count { id -> input.ranges.any { id in it } }.toString()
 
-    override fun part2(): String = ""
+
+    override fun part2(): String {
+        var counter: Long = 0
+        input.ranges.forEachIndexed { index, range ->
+            counter += if (index != 0) {
+                var rangeList = listOf(range)
+                (0 until index).forEach { oldIndex ->
+                    rangeList = rangeList.flatMap { currentRange ->
+                        currentRange.remove(input.ranges[oldIndex])
+                    }
+                }
+                rangeList.sumOf { (it.endInclusive - it.start) + 1 }
+            } else {
+                (range.endInclusive - range.start) + 1
+            }
+        }
+        return counter.toString()
+    }
 }
+
+fun LongRange.remove(rangeToRemove: LongRange): List<LongRange> {
+    if (this.first >= rangeToRemove.first && this.last <= rangeToRemove.last) return listOf()
+    if (this.first > rangeToRemove.last || this.last < rangeToRemove.first) return listOf(this)
+
+    return listOfNotNull(
+        takeIf { this.start < rangeToRemove.start }?.let {
+            LongRange(this.start, rangeToRemove.start - 1)
+        },
+        takeIf { this.endInclusive > rangeToRemove.endInclusive }?.let {
+            LongRange(
+                rangeToRemove.endInclusive + 1,
+                this.endInclusive
+            )
+        }
+    )
+}
+
+data class Input(val ranges: List<LongRange>, val ingredientIds: List<Long>)
--- src/test/kotlin/adventofcode2025/day5/Day5KtTest.kt
+++ src/test/kotlin/adventofcode2025/day5/Day5KtTest.kt
@@ -6,7 +6,17 @@ import org.junit.Test
 internal class Day5KtTest {
     private val program = ProgramDay5(
         brutInputs = listOf(
-            ""
+            "3-5",
+            "10-14",
+            "16-20",
+            "12-18",
+            "",
+            "1",
+            "5",
+            "8",
+            "11",
+            "17",
+            "32",
         ).map { it },
         debug = true
     )
@@ -14,7 +24,7 @@ internal class Day5KtTest {
     @Test
     fun part1() {
         assertEquals(
-            expected = "",
+            expected = "3",
             actual = program.part1()
         )
     }
@@ -22,7 +32,7 @@ internal class Day5KtTest {
     @Test
     fun part2() {
         assertEquals(
-            expected = "",
+            expected = "14",
             actual = program.part2()
         )
     }
